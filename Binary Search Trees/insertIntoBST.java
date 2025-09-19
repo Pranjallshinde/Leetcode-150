fd30227a023f92fa4d23d61225bf57433dfdde20
Pranjallshinde <141404018+Pranjallshinde@users.noreply.github.com>
2025-09-19 17:30:36 +0530

    Create insertIntoBST.java

---
+++ Binary Search Trees/insertIntoBST.java
@@ -0,0 +1,26 @@
+// Definition for a binary tree node
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        // If tree is empty, create a new node
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        // If value is smaller, go left
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } 
+        // If value is greater, go right
+        else if (val > root.val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        // If value already exists, we usually don't insert duplicates in BST
+        return root;
+    }
+}
