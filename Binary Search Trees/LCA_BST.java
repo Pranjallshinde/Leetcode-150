6f16cf360ec9412b776c0ba98c9bba5f11e9bdae
Pranjallshinde <141404018+Pranjallshinde@users.noreply.github.com>
2025-09-21 15:50:33 +0530

    Update LCA_BST.java

--- Binary Search Trees/LCA_BST.java
+++ Binary Search Trees/LCA_BST.java
@@ -1,10 +1,12 @@
-public static TreeNode lowestCommonAncestorRec(TreeNode root, TreeNode p, TreeNode q) {
-    if (root == null) return null;
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
 
-    if (p.val < root.val && q.val < root.val)
-        return lowestCommonAncestorRec(root.left, p, q);
-    else if (p.val > root.val && q.val > root.val)
-        return lowestCommonAncestorRec(root.right, p, q);
-    else
-        return root;
+        if (p.val < root.val && q.val < root.val)
+            return lowestCommonAncestor(root.left, p, q);
+        else if (p.val > root.val && q.val > root.val)
+            return lowestCommonAncestor(root.right, p, q);
+        else
+            return root; // Split point found
+    }
 }
